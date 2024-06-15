75b3afd365619f920f192b060d066034a2fd78ab
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-06-15 18:56:16 -0300

    feat: add the service implementation

---
+++ src/main/java/com/gg/SpringRestApi/service/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.gg.SpringRestApi.service;
+
+import com.gg.SpringRestApi.model.Product;
+import com.gg.SpringRestApi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
